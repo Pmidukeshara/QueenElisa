075862db3ac50464b46b91dd075d9d2d002e8e2f
Pamidukeshara123 <145844894+Pamidukeshara123@users.noreply.github.com>
2024-10-04 19:40:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: 'QUEENELISA;;;==wbk9EMtJDTvZVSfZnchpmZKFEM4YUWzQlNF1CVshXR5QVbs1WNY1ke6d1QjI0QLNleFNWRpwd=64102040749',
+  SESSION_ID: 'QUEENELISA;;;==AO0ZVZJBFMHx0amVVa19GVxVVTNJDcahXZMBFR5EmdWNEO0UEZwoUcipHejw0bDFVajlkWpwd=64102040749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
